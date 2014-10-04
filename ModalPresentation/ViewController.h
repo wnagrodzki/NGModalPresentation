c9a65e6df72fabf32bc5d0044c88cc027aa4d7bc
Wojciech Nagrodzki <w.nagrodzki@gmail.com>
2014-10-04 14:02:03 +0200

    Initial Commit

---
+++ ModalPresentation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModalPresentation
+//
+//  Created by Wojciech Nagrodzki on 04/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ModalPresentation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ModalPresentation
+//
+//  Created by Wojciech Nagrodzki on 04/10/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
