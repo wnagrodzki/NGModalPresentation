0e389c7028590ac9dd5b199ef3260d7ae29868a5
Wojciech Nagrodzki <278594+wnagrodzki@users.noreply.github.com>
2014-10-04 14:02:03 +0200

    Initial Commit

---
+++ ModalPresentation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ModalPresentation
+//
+//  Created by Wojciech Nagrodzki on 04/10/2014.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
